a06d61242a030002d674849332a52a3bbd144369
Artemiy <alianceap@mail.ru>
2024-09-20 23:09:48 +0300

    add some variables lesson1:task1

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 
-Console.WriteLine("Hello World!");
+// Console.WriteLine("Hello World!");
 // без using System dotnet run запускаться не хотел
 
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
