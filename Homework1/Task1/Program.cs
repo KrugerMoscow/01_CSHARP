5cd683af60f9e75b8414616b593c0c1ee5ecfed6
Artemiy <alianceap@mail.ru>
2024-09-23 21:11:47 +0300

    finish homework1:task1

--- Homework1/Task1/Program.cs
+++ Homework1/Task1/Program.cs
@@ -14,3 +14,23 @@
 На выходе:
 ● не делится */
 
+Console.Write("Введите первое целое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (secondNum == 0)
+{
+    Console.WriteLine("На 0 делить нельзя.");
+}
+
+else if (firstNum % secondNum == 0)
+{
+    Console.WriteLine($"{firstNum} делится на {secondNum} без остатка.");
+}
+
+else
+{
+    Console.WriteLine($"{firstNum} не делится на {secondNum} без остатка.");
+}
