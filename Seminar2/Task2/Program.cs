ae3ac2211b3bd0327514680c293d360a6cc29c0b
Artemiy <alianceap@mail.ru>
2024-09-24 23:09:13 +0300

    finish seminar2:task2

--- Seminar2/Task2/Program.cs
+++ Seminar2/Task2/Program.cs
@@ -6,3 +6,62 @@
 254 => 5^4 = 625
 617 => 1 */
 
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999) // для положительного числа
+{
+    int secondDigit = num / 10 % 10; // 487 / 10 = 48 (тк int дробная часть отбрасывается) % 10 = 4.8 ( % это остаток от деления, те 8)
+    int thirdDigit = num % 10; // 487 % 10 = 48.7 (получаем остаток от деления, те 7)
+
+    int result = secondDigit;
+    /*
+    Вариант 1: 
+    int count = 1; // Щетчик
+
+    while (count < thirdDigit) // не <= потому что result уже содержит одно значение
+    {
+        result *= secondDigit; // result = result * secondDigit
+        count++;
+    }
+    */
+
+    // Вариант 2:
+    for (int i = 1; i < thirdDigit; i++)
+    {
+        result *= secondDigit; // result = result * secondDigit
+    }
+
+    Console.WriteLine(result);
+}
+
+else if (num <= -100 && num >= -999) // для отрицательного числа
+{
+    int secondDigit = num / 10 % 10; // 487 / 10 = 48 (тк int дробная часть отбрасывается) % 10 = 4.8 ( % это остаток от деления, те 8)
+    int thirdDigit = num % 10; // 487 % 10 = 48.7 (получаем остаток от деления, те 7)
+
+    int result = secondDigit;
+    /*
+    Вариант 1: 
+    int count =-1; // Щетчик
+
+    while (count > thirdDigit) // не >= потому что result уже содержит одно значение
+    {
+        result *= secondDigit; // result = result * secondDigit
+        count--;
+    }
+    */
+
+    // Вариант 2:
+    for (int i = -1; i > thirdDigit; i--)
+    {
+        result *= secondDigit; // result = result * secondDigit
+    }
+
+    Console.WriteLine(result);
+}
+
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
