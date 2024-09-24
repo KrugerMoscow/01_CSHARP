1ded103184551698f1d6f2538c4a5785913fa19c
Artemiy <alianceap@mail.ru>
2024-09-24 21:45:42 +0300

    finish seminar2:task1

--- Seminar2/Task1/Program.cs
+++ Seminar2/Task1/Program.cs
@@ -4,3 +4,19 @@
 a = 256 => 26
 a = 891 => 81 */
 
+Console.Write("Введите целое трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999 || num <= -100 && num >= -999)
+{
+    int firstDigit = num / 100; // 256 / 100 = 2,56 , тк int целое число, то дробная часть отбрасывается
+    int thirdDigit = num % 10; // 256 % 10 = 250 + 6, 6 это остаток от деления 256 на 10, 256/10=25,6
+
+    int result = firstDigit * 10 + thirdDigit; // 2 * 10 + 6 = 26
+
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Некорректный ввод!");
+}
