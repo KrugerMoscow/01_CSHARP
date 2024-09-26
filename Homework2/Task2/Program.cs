2b77e646928eabbd8b49212c4278f4b793cf438e
Artemiy <alianceap@mail.ru>
2024-09-26 21:32:06 +0300

    finish homework2:task2

--- Homework2/Task2/Program.cs
+++ Homework2/Task2/Program.cs
@@ -2,3 +2,30 @@
 координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
 и выдаёт номер координатной четверти плоскости, 
 в которой находится эта точка. */
+
+Console.Write("Введите координату Х: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("Координата лежит в первой четверти.");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("Координата лежит во второй четверти.");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("Координата лежит в третьей четверти.");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("Координата лежит в четвертой четверти.");
+}
+else
+{
+    Console.WriteLine("Координата лежит в нуле.");
+}
