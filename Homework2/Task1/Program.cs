c98e03b9f174b40801795d83788c98cf0addb891
Artemiy <alianceap@mail.ru>
2024-09-26 21:07:03 +0300

    finish homework2:task1

--- Homework2/Task1/Program.cs
+++ Homework2/Task1/Program.cs
@@ -1,3 +1,14 @@
 ﻿/* Задача 1: Напишите программу, которая принимает на вход число и проверяет, 
 кратно ли оно одновременно 7 и 23. */
 
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine($"{num} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"{num} не кратно 7 и 23");
+}
