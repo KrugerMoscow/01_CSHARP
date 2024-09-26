533f427b4b7e0e0daace64825203080bc8c45353
Artemiy <alianceap@mail.ru>
2024-09-26 21:51:59 +0300

    finish homework2:task3

--- Homework2/Task3/Program.cs
+++ Homework2/Task3/Program.cs
@@ -2,3 +2,24 @@
 на вход целое число из отрезка [10, 99] и 
 показывает наибольшую цифру числа. */
 
+Console.Write("Введите число от 10 до 99: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10 && num <= 99)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine($"Большая цифра в числе {num} - {firstDigit}");
+    }
+    else
+    {
+        Console.WriteLine($"Большая цифра в числе {num} - {secondDigit}");
+    }
+}
+else
+{
+    Console.WriteLine("Число не входит в диапазон чисел от 10 до 99");
+}
