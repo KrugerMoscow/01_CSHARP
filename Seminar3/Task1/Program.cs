bb3d31fa28639d95492f7eb4744137756978b0b0
Artemiy <alianceap@mail.ru>
2024-09-28 18:51:03 +0300

    finish seminar3:task1

--- Seminar3/Task1/Program.cs
+++ Seminar3/Task1/Program.cs
@@ -5,3 +5,25 @@
 [1 3 4 19 3], 8 => Нет
 [-4 3 4 1], 3 => Да */
 
+// Варианты создания массива
+// int[] array1 = new int[5];
+// int[] array2 = new int[5] { 3, 6, 8, 3, 9 };
+// int[] array3 = new int[] { 3, 6, 8, 3, 9 };
+
+int[] array = { 1, 3, 4, 19, 3 };
+int num = 8;
+
+bool isExistNum = false; // есть ли в массиве число num
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break; // остановка цикла, если число в массиве есть, чтобы исключить множественное выведение значения при нескольких числах num
+    }
+}
+
+// string result = isExistNum ? "Да" : "Нет"; // Если isExistNum = true то выводить "да", иначе "нет"
+// Console.WriteLine(result); 
+Console.WriteLine(isExistNum ? "Да" : "Нет"); // тернарный (тройной) оператор - 10 > 5 ? "Да" : "Нет" - в качестве условия можно использовать любое логическое выражение
