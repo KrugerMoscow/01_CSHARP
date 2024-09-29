5c80fe7e92d5b71363e9be06bb164d853f7512e4
Artemiy <alianceap@mail.ru>
2024-09-29 20:53:32 +0300

    finish homework3:task3

--- Homework3/Task3/Program.cs
+++ Homework3/Task3/Program.cs
@@ -1,3 +1,17 @@
 ﻿/* Задание 3: Поиск среднего значения массива
 Описание: Задайте массив из 10 целых чисел. Найдите среднее значение элементов
 массива. */
+
+Console.Write("Введите массив чисел через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+
+double sum = 0; // среднее арифметическое может быть дробное число
+
+for (int i = 0; i < array.Length; i++)
+{
+    sum += array[i]; // sum = sum + array[i];
+}
+
+sum /= array.Length; // sum = sum / array.Length
+
+Console.WriteLine(sum);
