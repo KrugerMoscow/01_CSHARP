be47538b3fe6a490a55e0f06191cfe2fdc60b3e7
Artemiy <alianceap@mail.ru>
2024-09-29 19:29:46 +0300

    finish homework3:task1

--- Homework3/Task1/Program.cs
+++ Homework3/Task1/Program.cs
@@ -3,3 +3,25 @@
 присутствует ли заданное число в массиве. Программа должна вывести:
 Присутствует/Не присутствует. */
 
+
+Console.Write("Введите массив чисел через пробел: ");
+// посмотрел в интернете, считывает числа, отделяет пробелы, конвертирует в int, сохраняет в массив
+int[] array = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32); 
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool isExistNum = false; // Логический оператор для вывода ответа в консоль
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (num == array[i])
+    {
+        isExistNum = true;
+        break;
+    }
+}
+
+Console.Write($"Число {num} ");
+Console.Write(isExistNum ? "присутствует" : "не присутствует");
+Console.Write($" в массиве.");
