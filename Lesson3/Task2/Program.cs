152485314525024f1b73adccfca49bc0002dbab2
Artemiy <alianceap@mail.ru>
2024-09-30 22:07:41 +0300

    finish lesson3:task2

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿void PrintSquares(int limit) //функция ничего не возвращает, поэтому void, вывод в консоль - побочный эффект
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+}
+
+// Вызов функции
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
