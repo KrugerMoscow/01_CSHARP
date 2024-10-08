679f4740001169900edbdfc7f6dc43b223df97c1
Artemiy <alianceap@mail.ru>
2024-10-08 14:17:25 +0300

    finish lesson4:task3

--- Lesson4/Task3/Program.cs
+++ Lesson4/Task3/Program.cs
@@ -8,5 +8,19 @@
 
 string GetLettersFromString(string s)
 {
-    
+    string letters = "";
+    foreach (char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true) // метод определяет является ли перменная e буквой в таблице символов ASCII
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
 }
+
+// вызов метода
+
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+Console.WriteLine(result);
