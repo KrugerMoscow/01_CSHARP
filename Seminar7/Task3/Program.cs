8344c444c972c5388e67e6f336ff7274e277d343
Artemiy <alianceap@mail.ru>
2024-10-19 19:59:54 +0300

    seminar7:task3

---
+++ Seminar7/Task3/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Считать строку с консоли, содержащую латинские
+буквы. Вывести на экран согласные буквы этой
+строки.
+Указание
+Использовать рекурсию. Не использовать цикл.
+Пример
+“hello” => h l l
+“World” => W r l d
+“Hello world!” => H l l w r l d */
+
+// Метод для проверки на буквы и на согласные с последующей печатью
+
+void CheckPrintConsonants(string txt, int index)
+{
+    string vowels = "aeiouy";
+
+    if (char.IsAsciiLetter(txt[index]) && !vowels.Contains(txt[index])) // ! - оператор отрицания
+    {
+        Console.Write($"{txt[index]}");
+    }
+}
+
+// Метод с рекурсией, который проверяет не закончилась ли строка
+
+void PrintConsonants(string txt, int index = 0) // int index = 0 опциональный параметр
+{
+    if (index == txt.Length)
+    {
+        return;
+    }
+
+    CheckPrintConsonants(txt, index);
+    PrintConsonants(txt, index + 1);
+}
+
+Console.WriteLine("Введите строку, содержащую латинские буквы: ");
+string text = Console.ReadLine();
+PrintConsonants(text);
