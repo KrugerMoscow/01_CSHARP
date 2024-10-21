cbf0871be5e3027bfef09ca1456b943880b29160
Artemiy <alianceap@mail.ru>
2024-10-21 22:29:28 +0300

    add homework6:task2

---
+++ Homework6/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 2: Задайте строку, содержащую латинские буквы
+в обоих регистрах. Сформируйте строку, в которой все
+заглавные буквы заменены на строчные.
+Пример
+“aBcD1ef!-” => “abcd1ef!-” */
+
+
+string GetLowerLattersFromUpper(string str)
+{
+    string lowercaseString = string.Empty;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (char.IsUpper(str[i])) // Проверка на заглавные буквы
+        {
+            lowercaseString += char.ToLower(str[i]); // Изменение заглавных на строчные
+        }
+        else
+        {
+            lowercaseString += str[i];
+        }
+    }
+    return lowercaseString;
+}
+
+Console.WriteLine("Введите строку: ");
+string input = Console.ReadLine();
+string result = GetLowerLattersFromUpper(input);
+//string result = input.ToLower(); // Ну или вместо отдельного метода, использовать эту строчку
+Console.WriteLine(result);
