182a8573e5a0a9b913e3ca8be8f57524cce556fb
Artemiy <alianceap@mail.ru>
2024-10-21 23:03:02 +0300

    add homework6:task3

---
+++ Homework6/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задайте произвольную строку. Выясните,
+является ли она палиндромом.
+Пример:
+“aBcD1ef!-” => Нет
+“шалаш” => Да
+“55655” => Да */
+
+// Метод для проверки вводимой строки на палиндромность
+
+bool InputStringIsPalindrome(string str)
+{
+    int index = str.Length - 1;
+
+    for (int i = 0; i <= str.Length / 2; i++)
+    {
+        if (str[i] == str[index])
+        {
+            index--;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+Console.WriteLine("Введите строку: ");
+string input = Console.ReadLine();
+bool result = InputStringIsPalindrome(input);
+Console.Write(input + " => ");
+Console.WriteLine(result ? "Да" : "Нет"); // тернарный оператор
